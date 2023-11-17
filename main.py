395fb8c45f2d75df912e9cc5fce6d89bef67cdee
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2023-11-17 11:14:41 +0000

    Added comments to make it clear what all steps do

--- main.py
+++ main.py
@@ -3,43 +3,75 @@ import tkinter as tk
 root = tk.Tk()
 
 
-# A button to print something
+# This is a button that knows how to print something
+# It is just a tkinter Button which we then add extra bits to
+# The (tk.Button) line says we are inheriting from the tkinter class
 
-class button_that_prints(tk.Button):
-    def __init__(self, row, column):
+class ButtonThatPrints(tk.Button):
+
+    # Our __init__ has parameters for the button's row and column
+    # I've kept it simple, so any config will have to be done via "configure"
+    # (See later code)
+    def __init__(self, button_row, button_column):
+
+        # super() is the button's parent class - in this case tk.Button
+        # It needs to grab all the initialisation that a standard button would have
+
         super().__init__()
-        self.row = row
-        self.column = column
+
+        # Store the row and column in the button's properties
+
+        self.row = button_row
+        self.column = button_column
+
+    # This is the button's own, personal "clicked" method
 
     def clicked(self):
-        print(f"You clicked a button {(self.row, self.column)}... but I don't know which one.")
+        print(f"You clicked button {(self.row, self.column)}.")
 
-def print_something():
-    print('Bang')
+# We don't need this any more
+#
+#def print_something():
+#    print('Bang')
+
 
 # Let's create a grid of buttons...
+
+# I used variables to prove that the grid can have any size
 
 rows = 8
 columns = 8
 
+# Showing off by storing the buttons in a dictionary of dictionaries
+# (why not? it's fun)
+
+# The rows
 buttons = {}
 
 for i in range(rows):
+
+    # The cells in each row
     buttons[i] = {}
+
     for j in range(columns):
-        # Create button
+        # Create button and pass through its location
 
-        new_button = button_that_prints(row=i, column=j)
+        new_button = ButtonThatPrints(button_row=i, button_column=j)
 
+        # We then pass the button's own ButtonThatPrints.clicked() method as its own comman
         new_button.configure(command=new_button.clicked)
+
+        # (We can only do this once the button exists
+
+        # Store the button in my dictionary of dictionaries
 
         buttons[i][j] = new_button
 
-        # Place on grid
+        # Place on grid and let it fill its entry
 
         buttons[i][j].grid(row=i, column=j, sticky='news')
 
-# Describe rows and columns
+# Describe rows and columns - these will fill the screen
 
 for row in range(rows):
     root.rowconfigure(row, weight=1)
